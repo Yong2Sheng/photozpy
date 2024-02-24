dcec4fe2b281c1bf0fa6c34630a12826d1221a9e
Yong2Sheng <sheng2@clemson.edu>
2024-02-24 13:17:15 -0500

    Splitting the swiftz module, but not finished yet.

---
+++ src/photozpy/swiftz/swift_util.py
@@ -0,0 +1,29 @@
+"""
+Written by Yong Sheng at Clemson University, 2023 for the photozpy project.
+Advisor: Dr. Marco Ajello
+Other contributor(s):
+
+Main function: 
+- Download Swift data based on the quires.
+- Generate metadata of the data
+"""
+
+from pathlib import Path
+
+class Download():
+
+    def __init__(self, download_dir):
+
+        """
+        Initialize the class.
+        
+        Parameters
+        ----------
+        root_dir: pathlib.Path or str; the directory where the data will be stored.
+
+        Returns
+        -------
+        None
+        """
+
+        
