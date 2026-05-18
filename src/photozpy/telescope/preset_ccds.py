a096cc82f7d4fca23e314bda9e0ddc327492b152
Yong Sheng <sheng2@clemson.edu>
2026-05-18 11:45:40 -0400

    chores: pre-fix auto fix

--- src/photozpy/telescope/preset_ccds.py
+++ src/photozpy/telescope/preset_ccds.py
@@ -14,17 +14,17 @@ from .ccd import CCD
 
 RM_Andor = CCD(
     name="RM Andor Ikon-L",
-    gain=1.0 * u.electron/u.adu,
-    rdnoise=6.3 * u.electron/u.pix,
-    valid_from=date(2016,1,1),
+    gain=1.0 * u.electron / u.adu,
+    rdnoise=6.3 * u.electron / u.pix,
+    valid_from=date(2016, 1, 1),
     native_pixel_scale=0.34 * u.arcsec / u.pix,
 )
 
 CT_Andor = CCD(
     name="CT Andor Ikon-L",
-    gain=1.0 * u.electron/u.adu,
-    rdnoise=7.1 * u.electron/u.pix,
-    valid_from=date(2021,1,1),
+    gain=1.0 * u.electron / u.adu,
+    rdnoise=7.1 * u.electron / u.pix,
+    valid_from=date(2021, 1, 1),
     native_pixel_scale=0.34 * u.arcsec / u.pix,
 )
 
@@ -32,7 +32,7 @@ CT_FLI = CCD(
     name="CT FLI",
     gain=2.0 * u.electron / u.adu,
     rdnoise=(9.7 * 2.0) * u.electron / u.pix,
-    valid_from=date(2015,1,1),
-    valid_to=date(2020,12,31),
+    valid_from=date(2015, 1, 1),
+    valid_to=date(2020, 12, 31),
     native_pixel_scale=0.61 * u.arcsec / u.pix,
 )
